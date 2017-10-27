4b5b686df4041403e77c374263f80b8aeac50408
陆玄青 <lu.xuanqing@outlook.com>
2017-10-28 03:54:14 +0800

    return结果

--- tmall.py
+++ tmall.py
@@ -5,8 +5,8 @@ import re
 def getTmallPrice(url):
     # 检查url是否是字符串
     if not re.search('detail\.tmall\.com/(.*)[?&]id=(\d)+', url):
-        print('商品的天猫url错误')
-        return None
+        # print('商品的天猫url错误')
+        return '商品的天猫url错误'
 
     # 从url截取商品id
     regex = re.compile('[?&]id=(\d)+')
@@ -15,8 +15,8 @@ def getTmallPrice(url):
         id = match.group().split('=')[-1]
         print('id: {}'.format(id))
     else:
-        print('无法获取商品id')
-        return None
+        # print('无法获取商品id')
+        return '无法获取商品id'
 
     # http请求的request headers
     headers = {"Referer": url}
@@ -33,12 +33,14 @@ def getTmallPrice(url):
     try:
         price_info = result["defaultModel"]["itemPriceResultDO"]["priceInfo"]
         price = price_info["def"]["promotionList"][0]["price"]
-        print('price: {}'.format(price))
+        # print('price: {}'.format(price))
+        return 'price: {}'.format(price)
     except KeyError as e:
         print(e,'获取失败，请稍后重试')
+        return False
     
 
-getTmallPrice('https://detail.tmall.com/item.htm?id=20757312104')
-getTmallPrice('https://detail.tmall.com/item.htm?spm=a1z10.3-b-s.w4011-15096307578.48.751595225uUB1Q&id=525167180958&rn=27a3c84555e1eda097271292eb27af13&abbucket=11')
-getTmallPrice('https://detail.tmall.com/item.htm?spm=a220o.1000855.0.da321h.621b1e40ug4X52&id=22354227999')
-getTmallPrice('https://chaoshi.detail.tmall.com/item.htm?spm=a220m.1000858.1000725.5.d81179771fpxr&id=536571819737&areaId=310100&user_id=725677994&cat_id=2&is_b=1&rn=81f6294d7e67f238244f5882f43c506f')
+print(getTmallPrice('https://detail2.tmall.com/item.htm?id=20757312104'))
+print(getTmallPrice('https://detail.tmall.com/item.htm?spm=a1z10.3-b-s.w4011-15096307578.48.751595225uUB1Q&id=525167180958&rn=27a3c84555e1eda097271292eb27af13&abbucket=11'))
+print(getTmallPrice('https://detail.tmall.com/item.htm?spm=a220o.1000855.0.da321h.621b1e40ug4X52&id=22354227999'))
+print(getTmallPrice('https://chaoshi.detail.tmall.com/item.htm?spm=a220m.1000858.1000725.5.d81179771fpxr&id=536571819737&areaId=310100&user_id=725677994&cat_id=2&is_b=1&rn=81f6294d7e67f238244f5882f43c506f'))
