14b5f63fbeaea7ba62ba15ad5899b4e0131f056c
陆玄青 <lu.xuanqing@outlook.com>
2017-10-28 12:34:08 +0800

    完善错误返回代码

--- tmall.py
+++ tmall.py
@@ -3,10 +3,18 @@ import json
 import re
 
 def getTmallPrice(url):
+    '''输入一个String类型的参数（天猫商品的url），返回该商品的价格.
+
+    异常返回：
+    -1：商品的天猫url错误
+    -2：无法获取商品id
+    -3：发送的HTTP请求收不到返回数据
+    -4：天猫服务器没有返回正确数据'''
+    
     # 检查url是否是字符串
     if not re.search('detail\.tmall\.com/(.*)[?&]id=(\d)+', url):
         # print('商品的天猫url错误')
-        return '商品的天猫url错误'
+        return -1
 
     # 从url截取商品id
     regex = re.compile('[?&]id=(\d)+')
@@ -16,20 +24,23 @@ def getTmallPrice(url):
         print('id: {}'.format(id))
     else:
         # print('无法获取商品id')
-        return '无法获取商品id'
+        return -2
 
     # http请求的request headers
     headers = {"Referer": url}
     # 查询价格的地址
     queryURL = 'https://mdskip.taobao.com/core/initItemDetail.htm?itemId={}'.format(id)
 
-    # 发送请求
+    # 发送请求，将返回数据解析为json格式
     request = urllib.request.Request(queryURL, headers=headers)
     response = urllib.request.urlopen(request)
-    html = response.read().decode('gbk')
-    result = json.loads(html)
-    # print(type(result))
-    # print(result)
+    if response.status != 200:
+        print(response.status)
+        return -3
+    jsonStr = response.read().decode('gbk')
+    result = json.loads(jsonStr)
+
+    #从json结果中获取价格
     try:
         price_info = result["defaultModel"]["itemPriceResultDO"]["priceInfo"]
         price = price_info["def"]["promotionList"][0]["price"]
@@ -37,10 +48,11 @@ def getTmallPrice(url):
         return 'price: {}'.format(price)
     except KeyError as e:
         print(e,'获取失败，请稍后重试')
-        return False
+        return -4
     
 
-print(getTmallPrice('https://detail2.tmall.com/item.htm?id=20757312104'))
-print(getTmallPrice('https://detail.tmall.com/item.htm?spm=a1z10.3-b-s.w4011-15096307578.48.751595225uUB1Q&id=525167180958&rn=27a3c84555e1eda097271292eb27af13&abbucket=11'))
-print(getTmallPrice('https://detail.tmall.com/item.htm?spm=a220o.1000855.0.da321h.621b1e40ug4X52&id=22354227999'))
-print(getTmallPrice('https://chaoshi.detail.tmall.com/item.htm?spm=a220m.1000858.1000725.5.d81179771fpxr&id=536571819737&areaId=310100&user_id=725677994&cat_id=2&is_b=1&rn=81f6294d7e67f238244f5882f43c506f'))
+# print(getTmallPrice('https://detail.tmall.com/item.htm?id=20757312104'))
+# print(getTmallPrice('https://detail.tmall.com/item.htm?spm=a1z10.3-b-s.w4011-15096307578.48.751595225uUB1Q&id=525167180958&rn=27a3c84555e1eda097271292eb27af13&abbucket=11'))
+# print(getTmallPrice('https://detail.tmall.com/item.htm?spm=a220o.1000855.0.da321h.621b1e40ug4X52&id=22354227999'))
+# print(getTmallPrice('https://chaoshi.detail.tmall.com/item.htm?spm=a220m.1000858.1000725.5.d81179771fpxr&id=536571819737&areaId=310100&user_id=725677994&cat_id=2&is_b=1&rn=81f6294d7e67f238244f5882f43c506f'))
+print(getTmallPrice.__doc__)
