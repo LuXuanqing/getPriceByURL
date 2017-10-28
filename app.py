3a832e1e2b1c50ae5ce58e31743fbe19ab92cd7c
陆玄青 <lu.xuanqing@outlook.com>
2017-10-28 21:38:15 +0800

    url为空时跳过，不查询

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from openpyxl import load_workbook
 from openpyxl import Workbook
 from tmall import getTmallPrice
+from jd import getJdPrice
 
 # 加载excel文件中的第一个sheet
 wb = load_workbook('source.xlsx')
 ws = wb.worksheets[0]
 lenRows = len(list(ws.rows))
+print('共有{}行'.format(lenRows))
 # 获取除了第一行表头外的所有行
 rows = ws.iter_rows(row_offset=1, max_row=lenRows-1)
 
-for sku,name,url,price in rows:
-    # print(name)
-    price.value = getTmallPrice(url.value)
+for sku,name,jd_price,jd_url,tmall_price,tmall_url in rows:
+    print('正在查询：{0}-{1}'.format(sku.value, name.value))
+    if jd_url.value:
+        jd_price.value = getJdPrice(jd_url.value)
+    if tmall_url.value:
+        tmall_price.value = getTmallPrice(tmall_url.value)
 
 wb.save('result.xlsx')
 print('successfully saved')
